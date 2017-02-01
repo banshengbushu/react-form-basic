701178060ef0328bf039b4174997b8656b0b9ee4
banshengbushu <wangdanna1995@outlook.com>
2017-02-01 17:42:22 +0800

    Modify CSS

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -5,13 +5,14 @@ import LogicPuzzle from '../Components/LogicPuzzle.js';
 import SectionAddButton from '../Components/SectionAddButton.js';
 import PaperSubmit from '../Components/PaperSubmit.js';
 import SectionList from '../Components/SectionList.js';
+import '../../style/index.less'
 
 export default class CreatedPaper extends Component {
   render() {
     return (
       <div id="paper" className="col-md-10 col-md-offset-1 ">
         <div className="paper-header form-group">
-          <h4>新增试卷</h4>
+          <label>新增试卷</label>
         </div>
         <div className="paper-body">
           <PaperInfo/>
