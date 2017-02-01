641888c76acfe55b2083c14aa0cff5b7dadbc590
banshengbushu <wangdanna1995@outlook.com>
2017-02-01 14:53:37 +0800

    finished LogicPuzzle component

--- client/src/Components/LogicPuzzle.js
+++ client/src/Components/LogicPuzzle.js
@@ -1,10 +1,45 @@
 import {Component} from 'react';
 import React from 'react';
+import '../../style/paper-edit.less';
+import '../../style/index.less';
 
 export default class LogicPuzzle extends Component {
   render() {
     return (
-      <div>LogicPuzzle</div>
+      <div className="paper-logicPuzzle">
+        <div className="col-md-6 col-sm-offset-3">
+          <div className="row">
+            <div className="col-md-2">
+              <span className="pull-right">逻辑题</span>
+            </div>
+            <div className="col-md-6 form-group">
+              <input type="checkBox"/>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-3 col-sm-offset-4 easy-input">
+            <span className="col-sm-3">简单</span>
+            <div className="col-sm-3 no-padding form-group">
+              <input type="number" className="form-control"/>
+            </div>
+          </div>
+          <div className="col-sm-3 number-input">
+            <span className="col-sm-3">一般</span>
+            <div className="col-sm-3 no-padding form-group">
+              <input type="number" className="form-control"/>
+            </div>
+          </div>
+          <div className="col-sm-3 number-input">
+            <span className="col-sm-3">困难</span>
+            <div className="col-sm-3 no-padding form-group">
+              <input type="number" className="form-control"/>
+            </div>
+          </div>
+        </div>
+        <div className="paper-border">
+        </div>
+      </div>
     )
   }
 }
--- client/src/Components/PaperInfo.js
+++ client/src/Components/PaperInfo.js
@@ -6,18 +6,18 @@ export default class PaperInfo extends Component {
     return (
       <div className="col-md-6 col-md-offset-3">
         <div className="row">
-          <div className="paper-span col-md-2 pull-left">
-            <span>试卷名称</span>
+          <div className="paper-span col-md-2">
+            <span className="pull-right">试卷名称</span>
           </div>
-          <div className="col-md-9 form-group">
+          <div className="col-md-10 form-group">
             <input className="form-control" placeholder="请输入试卷名称"/>
           </div>
         </div>
         <div className="row">
-          <div className="paper-span col-md-2 pull-left">
-            <span>试卷描述</span>
+          <div className="paper-span col-md-2">
+            <span className="pull-right">试卷描述</span>
           </div>
-          <div className="col-md-9">
+          <div className="col-md-10 form-group">
             <input className="form-control" type="textArea" placeholder="请输入试卷描述"/>
           </div>
         </div>
