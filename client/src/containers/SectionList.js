015281356bb9e5b480d70f43b9ad6aaf6b188cc8
banshengbushu <wangdanna1995@outlook.com>
2017-02-03 22:49:26 +0800

    Finished SectionList

--- client/src/containers/SectionList.js
+++ client/src/containers/SectionList.js
@@ -3,11 +3,10 @@ import SectionList from '../components/SectionList';
 
 const mapStateToProps = (paperInfo)=> {
   let sections;
-  if (paperInfo.sections) {
-    sections = paperInfo.sections.filter(section => section.type === 'homeworkQuiz');
+  if (paperInfo.paperCreated.sections) {
+    sections = paperInfo.paperCreated.sections.filter(section => section.type === 'homeworkQuiz');
   }
   return {sections};
-
 };
 
 export default connect(mapStateToProps, ()=> {
